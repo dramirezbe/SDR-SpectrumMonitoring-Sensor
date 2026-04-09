3f0deaa9e4e9db521146fa0775aa274e521c4399
dramirezbe <dramirezbe@unal.edu.co>
2026-04-09 11:29:16 -0500

    Add idle script test

--- test/extract-csvs-nodes.py
+++ test/extract-csvs-nodes.py
@@ -3,6 +3,7 @@ import paramiko
 import os
 import stat
 
+# Configuración
 TUNNEL_HOST = "PLACEHOLDER"
 TUNNEL_PORT = 1222
 TUNNEL_USER = "root"
@@ -10,26 +11,35 @@ TUNNEL_PASS = 'PLACEHOLDER'
 
 NODE_PORT = 21104
 NODE_USER = "anepi"
-NODE_PASS = "PLACEHOLDER"
+NODE_PASS = "PLACEHOLDER" # Password proporcionada
 REMOTE_DIR = "/home/anepi/SDR-SpectrumMonitoring-Sensor/results"
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', required=True, type=str, help="Número de nodo")
-parser.add_argument('-r', required=True, type=str, help="Ruta local base")
+parser.add_argument('-n', type=str, help="Número de nodo (Modo Servidor)")
+parser.add_argument('-r', type=str, help="Ruta local base (Modo Servidor)")
+parser.add_argument('-ip', type=str, help="IP local (Modo Local)")
 args = parser.parse_args()
-
-node_ip = f"10.10.1.{args.n}"
-local_dir = os.path.join(args.r, f"Node{args.n}")
-
-jumpbox = paramiko.SSHClient()
-jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-jumpbox.connect(TUNNEL_HOST, port=TUNNEL_PORT, username=TUNNEL_USER, password=TUNNEL_PASS)
-
-channel = jumpbox.get_transport().open_channel("direct-tcpip", (node_ip, NODE_PORT), ('127.0.0.1', 0))
 
 target = paramiko.SSHClient()
 target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-target.connect(node_ip, port=NODE_PORT, username=NODE_USER, password=NODE_PASS, sock=channel)
+jumpbox = None
+
+if args.ip:
+    # MODO LOCAL
+    node_ip = args.ip
+    local_dir = args.ip
+    target.connect(node_ip, port=NODE_PORT, username=NODE_USER, password=NODE_PASS)
+else:
+    # MODO SERVIDOR (Túnel)
+    node_ip = f"10.10.1.{args.n}"
+    local_dir = os.path.join(args.r, f"Node{args.n}")
+    
+    jumpbox = paramiko.SSHClient()
+    jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    jumpbox.connect(TUNNEL_HOST, port=TUNNEL_PORT, username=TUNNEL_USER, password=TUNNEL_PASS)
+    
+    channel = jumpbox.get_transport().open_channel("direct-tcpip", (node_ip, NODE_PORT), ('127.0.0.1', 0))
+    target.connect(node_ip, port=NODE_PORT, username=NODE_USER, password=NODE_PASS, sock=channel)
 
 sftp = target.open_sftp()
 
@@ -50,4 +60,4 @@ print("Sincronización completada.")
 
 sftp.close()
 target.close()
-jumpbox.close()
+if jumpbox: jumpbox.close()
